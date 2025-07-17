c8da9d2beed2b65fcbb1ec2bcbf5104241a6a5a4
ayuintennurillah <ayuintennurillah08@gmail.com>
2025-07-17 16:46:05 +0700

    Ref: set session expired to login

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,16 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    Route::get('/agency', [App\Http\Controllers\AgencyController::class, 'index'])->name('agency');
+    Route::get('/agency/create', [App\Http\Controllers\AgencyController::class, 'create'])->name('agency.create');
+    Route::post('/agency', [App\Http\Controllers\AgencyController::class, 'store'])->name('agency.store');
+    Route::get('/agency/{id}', [App\Http\Controllers\AgencyController::class, 'edit'])->name('agency.edit');
+    Route::put('/agency/{id}/edit', [App\Http\Controllers\AgencyController::class, 'update'])->name('agency.update');
+    Route::delete('/agency/{id}', [App\Http\Controllers\AgencyController::class, 'destroy'])->name('agency.destroy');
+
+    Route::get('/merchandise', [App\Http\Controllers\MerchandiseController::class, 'index'])->name('merch');
+});
